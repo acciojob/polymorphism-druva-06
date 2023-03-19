16be5e0232b746be9631c51fe894bde7441857b2
Druvasena Reddy Naini <nainidruvasenareddy3737@gmail.com>
2023-03-19 14:13:44 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-
+    class Product{
+        public int product(int x,int y){ return x*y; }
+        public int product(int x,int y,int z){ return x*y*z; }
+        public double product(double x,double y){ return x*y; }
+    }
+    public static void main(String args[]){
+        Product p = new Main().new Product();
+        int ans1 = p.product(2,3);
+        int ans2 = p.product(2,3,4);
+        double ans3 = p.product(2.5,3.0);
+        System.out.println(ans1+" "+ans2+" "+ans3);
+    }
 }
