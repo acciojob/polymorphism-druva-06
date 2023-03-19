fe570bd719340b4a3ee575066fdf4ae209830acd
Druvasena Reddy Naini <nainidruvasenareddy3737@gmail.com>
2023-03-19 14:22:54 +0530

    Modified

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,13 +1,13 @@
 package com.driver;
 
 public class Main {
-    class Product{
+    public static class Product{
         public int product(int x,int y){ return x*y; }
         public int product(int x,int y,int z){ return x*y*z; }
         public double product(double x,double y){ return x*y; }
     }
     public static void main(String args[]){
-        Product p = new Main().new Product();
+        Product p = new Product();
         int ans1 = p.product(2,3);
         int ans2 = p.product(2,3,4);
         double ans3 = p.product(2.5,3.0);
